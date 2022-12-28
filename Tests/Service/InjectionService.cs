0cf71ccb2fb3cb5a3ee6627c7b706056f4f7f2af
Ram Pathakota <ram.pathakota@katoennatie.com>
2022-12-28 12:51:52 +0100

    feat: My first feature

---
+++ Tests/Service/InjectionService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Data;
+
+namespace Tests.Service
+{
+    public static class InjectionService
+    {
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<AppDbContext>();
+            return services;
+        }
+    }
+}
